42feed5f5ee090f39a7c450e2ba4e4b2da54b9e0
Christian Bell <camaribell@yahoo.com>
2023-04-16 10:33:47 -0700

    Done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Worlds Meet Gang')
 })
 
 app.get('*', (req, res) => {
