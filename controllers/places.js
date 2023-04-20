47bd072028725b88eaaa090a3ea0671ae481f814
Christian Bell <camaribell@yahoo.com>
2023-04-19 21:00:26 -0700

    adding rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,30 @@
 const router = require("express").Router();
 
 // GET /places
-router.get('/', (req, res) => {
-    let places = []
-    res.render('places/index', { places })
-})
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-  }]
-  
-  
+router.get("/", (req, res) => {
+  let places = [];
+  res.render("places/index", { places });
+});
+
+let places = [
+  {
+    name: "H-Thai-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "http://placekitten.com/250/250",
+  },
+  {
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "http://placekitten.com/250/250",
+  },
+];
+
 module.exports = router;
