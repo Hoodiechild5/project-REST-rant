1573124511ee68b1dda526219e5f57afa23dee82
Christian Bell <camaribell@yahoo.com>
2023-05-04 15:40:24 -0700

    made changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const db = require('../models')
+const db = require('../models/places')
 
 router.get('/', (req, res) => {
     db.Place.find()
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -9,25 +9,25 @@ function new_form(data) {
         <form method="POST" action="/places">
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
-            <input type="url" className="form-control" id="name" name="name" required />
+            <input type="text" className="form-control" id="name" name="name" required />
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
-            <input type="url" className="form-control" id="pic" name="pic" />
+            <input type="text" className="form-control" id="pic" name="pic" />
           </div>
           <div className="form-group">
             <label htmlFor="city">City</label>
-            <input type="url" className="form-control" id="city" name="city" />
+            <input type="text" className="form-control" id="city" name="city" />
           </div>
           <div className="form-group">
             <label htmlFor="state">State</label>
-            <input type="url" className="form-control" id="state" name="state" />
+            <input type="text" className="form-control" id="state" name="state" />
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
-            <input type="url" className="form-control" id="cuisines" name="cuisines" required />
+            <input type="text" className="form-control" id="cuisines" name="cuisines" required />
           </div>
-          <div className="form-group col-sm-4">
+          <div className="form-group">
             <label htmlFor="founded">Founded Year</label>
             <input
               type="number"
